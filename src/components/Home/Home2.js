6d97df2ccdf3f3fa854937cf7c9490ccdadf7655
udhayavana <udhayavana@gmail.com>
2024-03-03 17:46:25 +0530

    social links correction 2

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/udhayavanan"
+                  href="https://github.com/udhayavana"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"

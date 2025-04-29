ea953f6b653a080ad5b397256ffdcf0266016547
Udhayavanan Sambath <udhayavana@gmail.com>
2025-04-29 22:31:04 +0530

    nav bar menu show hide 1

--- src/layout/Default.jsx
+++ src/layout/Default.jsx
@@ -1,50 +1,11 @@
 import { Outlet, Link } from "react-router";
 import logo from '../img/my-profile-img.jpg';
-import { useEffect, useState } from "react";
-
-const getDeviceType = (width) => {
-  if (width < 768) return 'Mobile';
-  if (width >= 768 && width < 1024) return 'Tablet';
-  return 'Desktop';
-};
-
-
 const Default = () => {
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [deviceType, setDeviceType] = useState(getDeviceType(window.innerWidth));
-
-  const handleToggle = () => {
-    setIsOpen(prev => !prev);
-    const width = window.innerWidth;
-    setWindowWidth(width);
-    setDeviceType(getDeviceType(width));
-  };
-  
-  useEffect(() => {
-    
-    const handleResize = () => {
-      const width = window.innerWidth;
-      setWindowWidth(width);
-      setDeviceType(getDeviceType(width));
-      if(deviceType!=="Desktop"){
-        setIsOpen(true);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // Cleanup
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   return (
     <>
-      <header id="header" className={`header dark-background d-flex flex-column ${ (deviceType==="Desktop"|| isOpen) ? 'header-show':''}`}>
-        <i  onClick={handleToggle} className={`header-toggle d-xl-none bi ${isOpen ? 'bi-x' : 'bi-list'}`}></i>
+      <header id="header" className="header dark-background d-flex flex-column">
+        <i className="header-toggle d-xl-none bi bi-list"></i>
         <div className="profile-img">
           <img src={logo} alt="" className="img-fluid square" />
         </div>
